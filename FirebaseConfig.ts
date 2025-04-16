bcb569ef2e410235c7c230e92aa09f263abb6db2
Yi Ya <tanyiya04@gmail.com>
2025-04-16 10:05:40 +0800

    Update FirebaseConfig.ts

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,1 +1,19 @@
-${{ secrets.FIREBASECONFIG }}
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: ${{ secrets.FIREBASECONFIG }},
+  authDomain: "meni-reactnative.firebaseapp.com",
+  projectId: "meni-reactnative",
+  storageBucket: "meni-reactnative.firebasestorage.app",
+  messagingSenderId: "296699811775",
+  appId: "1:296699811775:web:4165deb1712d670610b30a"
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
